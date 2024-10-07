bf63b66e4025ad4edea70b4a71a803d5d2f3dede
farhanakhtar24 <akhtar.farhan779@gmail.com>
2024-10-07 12:24:56 +0530

    bug fix

--- middleware.ts
+++ middleware.ts
@@ -19,6 +19,10 @@ export default async function middleware(request: NextRequest) {
   return NextResponse.next();
 }
 export const config = {
-  matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
-  runtime: "nodejs",
+  matcher: [
+    // Skip Next.js internals and all static files, unless found in search params
+    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
+    // Always run for API routes
+    "/(api|trpc)(.*)",
+  ],
 };
